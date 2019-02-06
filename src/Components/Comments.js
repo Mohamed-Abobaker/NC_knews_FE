34cf3dc0106a2424975d01db3a59bb782c29eea6
Mohamed-Abobaker <mohamedabobaker17@gmail.com>
2019-02-06 14:10:15 +0000

    added a comment component that allows the votes to be adjusted

---
+++ src/Components/Comments.js
@@ -0,0 +1,51 @@
+import React, { Component } from "react";
+import axios from "axios";
+
+class Comments extends Component {
+  state = {
+    comment: this.props.comment,
+    commentModifier: 0
+  };
+
+  render() {
+    const { comment, commentModifier } = this.state;
+    return (
+      <div>
+        <p>
+          {comment.created_at.substring(0, 10)} ----- {comment.author} commented
+          :<br />'{comment.body}'
+        </p>
+        <p>Comment Votes : {comment.votes + commentModifier}</p>
+        <button
+          type="button"
+          onClick={() => this.voteToComment(comment.comment_id, 1)}
+        >
+          Like
+        </button>
+        <button
+          type="button"
+          onClick={() => this.voteToComment(comment.comment_id, -1)}
+        >
+          Dislike
+        </button>
+      </div>
+    );
+  }
+
+  voteToComment = (comm_id, num) => {
+    const { id } = this.props;
+    const add = { inc_votes: num };
+    axios
+      .patch(
+        `https://nc-knews777.herokuapp.com/api/articles/${id}/comments/${comm_id}`,
+        add
+      )
+      .then(({ data }) => {
+        this.setState({
+          commentModifier: this.state.commentModifier + num
+        });
+      });
+  };
+}
+
+export default Comments;
--- src/Components/SingleArticle.js
+++ src/Components/SingleArticle.js
@@ -1,15 +1,16 @@
 import React, { Component } from "react";
 import axios from "axios";
+import Comments from "./Comments";
 
 class SingleArticle extends Component {
   state = {
     article: null,
-    votes: null,
+    votesModifier: 0,
     comments: null,
     test: null
   };
   render() {
-    const { article, votes, comments } = this.state;
+    const { article, votesModifier, comments } = this.state;
     return (
       <div>
         <div>
@@ -21,11 +22,11 @@ class SingleArticle extends Component {
           </p>
         </div>
         <div>
-          <p>Article Votes: {votes && votes}</p>
-          <button type="button" onClick={this.likeToArticle}>
+          <p>Article Votes: {article && article.votes + votesModifier}</p>
+          <button type="button" onClick={() => this.voteToArticle(1)}>
             Like
           </button>
-          <button type="button" onClick={this.dislikeToArticle}>
+          <button type="button" onClick={() => this.voteToArticle(-1)}>
             Dislike
           </button>
         </div>
@@ -34,25 +35,11 @@ class SingleArticle extends Component {
           {comments &&
             comments.map(comment => {
               return (
-                <div key={comment.created_at}>
-                  <p>
-                    {comment.created_at.substring(0, 10)} ----- {comment.author}{" "}
-                    commented :<br />'{comment.body}'
-                  </p>
-                  <p>Comment Votes : {comment.votes}</p>
-                  <button
-                    type="button"
-                    onClick={() => this.likeComment(comment.comment_id)}
-                  >
-                    Like
-                  </button>
-                  <button
-                    type="button"
-                    onClick={() => this.dislikeComment(comment.comment_id)}
-                  >
-                    Dislike
-                  </button>
-                </div>
+                <Comments
+                  key={comment.comment_id}
+                  id={article && article.article_id}
+                  comment={comment}
+                />
               );
             })}
         </div>
@@ -88,58 +75,14 @@ class SingleArticle extends Component {
       });
   };
 
-  likeToArticle = () => {
+  voteToArticle = vote => {
     const { id } = this.props;
-    const add = { inc_votes: 1 };
+    const add = { inc_votes: vote };
     axios
       .patch(`https://nc-knews777.herokuapp.com/api/articles/${id}`, add)
       .then(({ data }) => {
         this.setState({
-          votes: data.article.votes
-        });
-      });
-  };
-
-  dislikeToArticle = () => {
-    const { id } = this.props;
-    const minus = { inc_votes: -1 };
-    axios
-      .patch(`https://nc-knews777.herokuapp.com/api/articles/${id}`, minus)
-      .then(({ data }) => {
-        this.setState({
-          votes: data.article.votes
-        });
-      });
-  };
-
-  likeComment = comm_id => {
-    const { id } = this.props;
-    const add = { inc_votes: 1 };
-    axios
-      .patch(
-        `https://nc-knews777.herokuapp.com/api/articles/${id}/comments/${comm_id}`,
-        add
-      )
-      .then(({ data }) => {
-        console.log("data", data);
-        this.setState({
-          test: data
-        });
-      });
-  };
-
-  dislikeComment = comm_id => {
-    const { id } = this.props;
-    const add = { inc_votes: -1 };
-    axios
-      .patch(
-        `https://nc-knews777.herokuapp.com/api/articles/${id}/comments/${comm_id}`,
-        add
-      )
-      .then(({ data }) => {
-        console.log("data", data);
-        this.setState({
-          test: data
+          votesModifier: this.state.votesModifier + vote
         });
       });
   };
