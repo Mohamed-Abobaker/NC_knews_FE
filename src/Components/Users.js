44a9084af9d6c4e76d51f659d91eca609da5d60f
Mohamed-Abobaker <mohamedabobaker17@gmail.com>
2019-02-06 10:02:33 +0000

    added a users component and page

--- src/Components/Users.js
+++ src/Components/Users.js
@@ -1,9 +1,50 @@
 import React, { Component } from "react";
+import axios from "axios";
 
 class Users extends Component {
+  state = {
+    users: []
+  };
   render() {
-    return <div>Users</div>;
+    const { users } = this.state;
+    console.log(users);
+    return (
+      <div>
+        <h1>Users</h1>{" "}
+        {users.map(user => {
+          return (
+            <div key = {user.username}>
+              <img
+                src={user.avatar_url}
+                alt="Avatar pic"
+                height="100"
+                width="100"
+              />
+              <p>
+                Name : {user.name}
+                <br />
+                Username: {user.username}{" "}
+              </p>
+            </div>
+          );
+        })}
+      </div>
+    );
   }
+
+  componentDidMount() {
+    this.getUsers();
+  }
+
+  getUsers = () => {
+    axios
+      .get("https://nc-knews777.herokuapp.com/api/users")
+      .then(({ data }) => {
+        this.setState({
+          users: data.users
+        });
+      });
+  };
 }
 
 export default Users;
