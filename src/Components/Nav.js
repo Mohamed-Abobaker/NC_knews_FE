28f74392a876207e2e9e356d5d912cc1d7df97bd
Mohamed-Abobaker <mohamedabobaker17@gmail.com>
2019-02-12 11:15:58 +0000

    update Nav component to class

--- src/Components/Nav.js
+++ src/Components/Nav.js
@@ -1,29 +1,33 @@
-import React from "react";
+import React, { Component } from "react";
 import { Link } from "@reach/router";
 import "../App.css";
 
-const Nav = () => {
-  return (
-    <div className="topNav">
-      <h1 className="page-title">Northcoder News</h1>
-      <div className="navBar">
-        <nav>
-          <Link to="/">Home</Link>
-          {" | "}
-          <Link to="/articles">Articles</Link>
-          {" | "}
-          <Link to="/topics">Topics</Link>
-          {" | "}
-          <Link to="/users">Users</Link>
-        </nav>
-        <div>
-          <button type="button" onClick={() => this.props.logoutFunc()}>
-            Logout
-          </button>
+class Nav extends Component {
+  render() {
+    // const { logoutFunc } = this.props;
+    // console.log(this.props);
+    return (
+      <div className="topNav">
+        <h1 className="page-title">Northcoder News</h1>
+        <div className="navBar">
+          <nav>
+            <Link to="/">Home</Link>
+            {" | "}
+            <Link to="/articles">Articles</Link>
+            {" | "}
+            <Link to="/topics">Topics</Link>
+            {" | "}
+            <Link to="/users">Users</Link>
+          </nav>
+          <div>
+            <button type="button" onClick={() => this.props.logoutFunc()}>
+              Logout
+            </button>
+          </div>
         </div>
       </div>
-    </div>
-  );
-};
+    );
+  }
+}
 
 export default Nav;
